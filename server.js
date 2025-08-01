97329d249207272dd1ebd6c9c00de5d15e0588f1
riyadmaj <lcodehowa@gmail.com>
2025-08-01 19:11:30 +0100

    vercem

--- server.js
+++ server.js
@@ -50,4 +50,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`));//
